6cdd6c30b2a94d1aab088aa199296e5283a48697
Jacob Feddersen <jtfedd@gmail.com>
2024-01-06 12:20:53 -0600

    Not going to use the version where we pass bytes in

--- src/bindings.rs
+++ src/bindings.rs
@@ -8,14 +8,6 @@ use crate::decompress::decompress;
 use crate::convert::convert_nexrad_file;
 use crate::model::PyLevel2File;
 
-/// Decodes a nexrad file given bytes
-#[pyfunction]
-fn parse_nexrad_file(bytes: Vec<u8>) -> PyLevel2File {
-    let decompressed = decompress(bytes);
-    let decoded = decode_file(&decompressed).expect("decodes file");
-    convert_nexrad_file(&decoded)
-}
-
 /// Downloads and decodes a nexrad file
 #[pyfunction]
 fn download_nexrad_file(
@@ -35,13 +27,14 @@ fn download_nexrad_file(
         download_file(&FileMetadata::new(site.to_string(), date, identifier.to_string())).await
     }).expect("Should download without error");
 
-    parse_nexrad_file(bytes)
+    let decompressed = decompress(bytes);
+    let decoded = decode_file(&decompressed).expect("decodes file");
+    convert_nexrad_file(&decoded)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pynexrad(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(parse_nexrad_file, m)?)?;
     m.add_function(wrap_pyfunction!(download_nexrad_file, m)?)?;
     m.add_class::<PyLevel2File>()?;
     Ok(())
